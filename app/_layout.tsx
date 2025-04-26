21ed93bdef01c288263a9828a6a44badedab90f4
cyrus_w <cyrusw89@gmail.com>
2025-04-26 17:02:56 +1200

    setup: routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 
-import "./global.css";
+import "./globals.css";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
